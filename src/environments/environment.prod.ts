a3852db54a0b5b101f84520b2502c270af90fff3
Caleb Brenner <calebwill91@gmail.com>
2020-04-18 04:08:29 -0500

    Set up components, routing, firebase and AngularFire2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBbpFwkJChGyMcwGwQPjpa4ZPBicLUn9RE',
+    authDomain: 'clientpanelprod-cd2b0.firebaseapp.com',
+    databaseURL: 'https://clientpanelprod-cd2b0.firebaseio.com',
+    projectId: 'clientpanelprod-cd2b0',
+    storageBucket: 'clientpanelprod-cd2b0.appspot.com',
+    messagingSenderId: '566913337533',
+    appId: '1:566913337533:web:3cbf52d1e6fdb61dcc8fd4',
+    measurementId: 'G-1YKWQFDFZ3',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,14 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBbpFwkJChGyMcwGwQPjpa4ZPBicLUn9RE',
+    authDomain: 'clientpanelprod-cd2b0.firebaseapp.com',
+    databaseURL: 'https://clientpanelprod-cd2b0.firebaseio.com',
+    projectId: 'clientpanelprod-cd2b0',
+    storageBucket: 'clientpanelprod-cd2b0.appspot.com',
+    messagingSenderId: '566913337533',
+    appId: '1:566913337533:web:3cbf52d1e6fdb61dcc8fd4',
+    measurementId: 'G-1YKWQFDFZ3',
+  },
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
